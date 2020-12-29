3678dc3f565b03cbe42b2378f2a22d536cf81620
Andrey <zylkovsliy.andrey@mail.ru>
2020-12-29 19:52:26 +0300

    Вёрстка, написаны основные классы и их функционал (js)

--- js/script.js
+++ js/script.js
@@ -1,169 +1,167 @@
-class ProductsList{
-    constructor(container = '.products'){
+class List {
+    constructor (container, render= data => '') {
         this.container = container;
-        this.goods = [];
+        this.items = new Map();
+        this.render_item = render;
     }
 
-    fetchProducts(products_list) {
-        this.goods = products_list.map(product => new ProductItem(product));
+    addItem(item) {
+        let id = item.id;
+        if (this.items.has(id))
+            this.items.get(id).quantity++;
+        else
+            this.items.set(id, new ListItem(item));
+    }
+
+    delItem(item) {
+        let id = item.id;
+        if (this.items.has(id)) {
+            let product = this.items.get(id);
+            if (product.quantity > 0)
+                product.quantity--;
+        }
+    }
+
+    renderSingle(item) {
+        document.querySelector(this.container).insertAdjacentHTML('beforeend',  this.render_item(item));
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        this.items.forEach((value) => {
+            if (value.quantity > 0)
+                block.insertAdjacentHTML('beforeend',  this.render_item(value))
+        });
+    }
+}
+
+
+class ListItem {
+    constructor (data, initial_quantity=1) {
+        this.id = data.id;
+        this.title = data.title;
+        this.price = data.price;
+        this.quantity = initial_quantity;
+    }
+}
+
+
+class ProductsList extends List {
+    constructor(container='.products'){
+        super(container, data =>`<div class="item">
+                                    <p class="item__name">${data.title}</p>
+                                    <p class="item__price">${data.price}$</p>
+                                    <button class="item__buy-btn" item-id="${data.id}">Buy</button>
+                                </div>`);
+        this.cart = undefined;
+
+        document.querySelector(this.container).addEventListener('click', event => {
+            if (event.target.classList.contains('item__buy-btn') && this.cart) {
+                let id = Number(event.target.getAttribute('item-id'));
+                this.cart.addItem(this.items.get(id));
+            }
+        });
+    }
+
+    _fetchProducts(url) {
+        return fetch(url)
+            .then(json_data => json_data.json());
+    }
+
+    load(url) {
+        this._fetchProducts(url)
+            .then(data => {
+                data.forEach(item_data => {
+                    item_data = {
+                        id: item_data.id_product,
+                        title: item_data.product_name,
+                        price: item_data.price
+                    };
+                    this.addItem(new ListItem(item_data));
+                });
+                this.render();
+            })
+            .catch(() => document.querySelector(this.container).insertAdjacentHTML('beforebegin', '<p class="error-msg">An error has occurred<p/>'))
+    }
+
+    bindCart(cart) {
+        this.cart = cart;
     }
 
     calcProductsPrice() {
         return this.goods.reduce((a, b) => a + b.price, 0);
     }
-    
-    render() {
-        const block = document.querySelector(this.container);
-        for (let product of this.goods) {
-            block.insertAdjacentHTML('beforeend', product.render());
-        }
-    }
 }
 
 
-class ProductItem {
-    constructor(product) {
-        this.title = product.title;
-        this.price = product.price;
-        this.id = product.id;
+class Cart extends List {
+    constructor (container='.cart__container') {
+        super(container, data => `<div class="cart-item" data-id="${data.id}">
+                                    <div class="product-bio">
+                                    <div class="product-desc">
+                                    <p class="product-title">${data.title}</p>
+                                    <p class="product-quantity">Quantity: ${data.quantity}</p>
+                                <p class="product-single-price">$${data.price} each</p>
+                                </div>
+                                </div>
+                                <div class="right-block">
+                                    <p class="product-price">$${data.quantity * data.price}</p>
+                                    <button class="del-btn" data-id="${data.id}">&times;</button>
+                                </div>
+                                </div>`)
+
+        document.querySelector('.cart-btn').addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('invisible');
+        });
+        document.querySelector(this.container).addEventListener('click', event => {
+            if (event.target.classList.contains('del-btn')) {
+                let id = Number(event.target.getAttribute('data-id'));
+                this.delItem(this.items.get(id));
+            }
+        });
     }
 
-    render() {
-        return  `<div class="item"> 
-                    <p class="item__name">${this.title}</p> 
-                    <p class="item__price">${this.price}$</p> 
-                    <button class="item__buy-btn">Buy</button> 
-                </div>`;
+    addItem(item) {
+        super.addItem(item);
+        item = this.items.get(item.id);
+        if (item.quantity == 1)
+            this.renderSingle(item);
+        else
+            this._updateItem(item);
     }
-}
 
+    delItem(item) {
+        super.delItem(item);
+        item = this.items.get(item.id);
+        if (item.quantity == 0)
+            this._deleteItem(item);
+        else
+            this._updateItem(item);
+    }
 
+    _findItem(id) {
+        return document.querySelector(`.cart-item[data-id="${id}"]`);
+    }
 
-/*********************************************************************/
-// Методы, описанные в ProductsList сюда тоже подходят
-class ShoppingCart extends ProductsList {
-    addItem() {}
-    delItem() {}
+    _updateItem(item) {
+        let block = this._findItem(item.id);
+        block.querySelector('.product-quantity').textContent = `Quantity: ${item.quantity}`;
+        block.querySelector('.product-price').textContent = `$${item.quantity * item.price}`;
+    }
+
+    _deleteItem(item) {
+        this._findItem(item.id).remove()
+    }
+
     selectItem() {}
     filter() {}
     makeOrder() {}
 }
 
 
-class ShoppingCartItem extends ProductItem {
-    render() {}
-}
-
-
-
-/*********************************************************************/
-class FoodInfo {
-    constructor (price, kcal, name) {
-        this.name = name;
-        this.price = price;
-        this.kcal = kcal;
-    }
-
-    getInfo() {
-        return `${this.name} price: ${this.price} kcal: ${this.kcal}`;
-    }
-}
-
-
-class Hamburger {
-    static sizes = {
-        'small' : new FoodInfo(50, 20, 'small'),
-        'big' : new FoodInfo(100, 40, 'big')
-    }
-
-    static stuffings = {
-        'cheese' : new FoodInfo(10, 20, 'cheese'),
-        'salad' : new FoodInfo(20, 5, 'salad'),
-        'potato' : new FoodInfo(15, 10, 'potato')
-    }
-
-    static toppings = {
-        'condiment': new FoodInfo(15, 0, 'condiment'),
-        'mayonnaise': new FoodInfo(20, 5, 'mayonnaise')
-    }
-
-
-    constructor(size, stuffing) {
-        this.size = size;
-        this.stuffing = stuffing;
-        this.toppings_list = [];
-    }
-
-    addTopping(topping) {
-        // Добавить добавку
-        this.toppings_list.push(topping);
-    }
-
-    removeTopping(topping) {
-        // Убрать добавку
-        let index = this.toppings_list.indexOf(topping);
-        if (index != -1)
-            this.toppings_list.splice(index, 1);
-    }
-
-    getToppings() {
-        // Узнать информацию о добавках
-        return this.toppings_list.map(item => item.getInfo()).join('\n');
-    }
-
-    getSize() {
-        // Узнать размер гамбургера
-        return this.size.getInfo();
-    }
-
-    changeSize(size) {
-        // Изменить размер гамбургера
-        this.size = size;
-    }
-
-    getStuffing() {
-        // Узнать начинку гамбургера
-        return this.stuffing.getInfo();
-    }
-
-    changeStuffing(stuffing) {
-        // Изменить начинку гамбургера
-        this.stuffing = stuffing;
-    }
-
-    calculatePrice() {
-        // Узнать цену
-        return this.size.price + this.stuffing.price + this.toppings_list.reduce((a, b) => a + b.price, 0);
-    }
-    
-    calculateCalories() {
-        // Узнать калорийность
-        return this.size.kcal + this.stuffing.kcal + this.toppings_list.reduce((a, b) => a + b.kcal, 0);
-    }
-  }
-
-
-
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
 let list = new ProductsList();
+let cart = new Cart();
 
-list.fetchProducts(products);
-list.render();
-alert(`Суммарная цена всех товаров: ${list.calcProductsPrice()}`);
-
-
-
-
-let hm = new Hamburger(Hamburger.sizes.big, Hamburger.stuffings.potato);
-hm.addTopping(Hamburger.toppings.condiment);
-hm.addTopping(Hamburger.toppings.condiment);
-hm.removeTopping(Hamburger.toppings.condiment);
-hm.addTopping(Hamburger.toppings.mayonnaise);
-hm.changeSize(Hamburger.sizes.small);
-hm.changeStuffing(Hamburger.stuffings.salad);
-alert(`Размер: ${hm.getSize()}\nНачинка: ${hm.getStuffing()}\nДобавки: ${hm.getToppings()}\nЦена: ${hm.calculatePrice()}\nКалорийность: ${hm.calculateCalories()}`);
+list.load(API);
+list.bindCart(cart);
